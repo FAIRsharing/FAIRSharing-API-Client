753b47a08f61f37f26ce72288877357fad551577
Terazus <batistadominique@hotmail.com>
2021-10-15 05:07:13 -0700

    adding map files to packing

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-module.exports = require("./src/client")
+module.exports = {
+    ...require("./src/queries/authentication"),
+    ...require("./src/queries/getters"),
+    ...require("./src/queries/setters"),
+    ...require('./src/queries/validation'),
+    ...require('./src/queries/helpers'),
+    ...require('./src/core/cache'),
+    setURL: (url) => { require('./src/core/client.conf').baseURL = url }
+}
--- src/client.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    ...require("./queries/authentication"),
-    ...require("./queries/getters"),
-    ...require("./queries/setters"),
-    ...require('./queries/validation'),
-    ...require('./queries/helpers'),
-    ...require('./core/cache'),
-    setURL: (url) => { require('./core/client.conf').baseURL = url }
-}
